52c10dff7775e9127cdd4fbfcdfe62f66303bf63
yujong-lee <yujonglee@g.skku.edu>
2021-07-31 15:46:44 +0900

    karma로 jsdom에서의 테스트 환경을 구축하라

--- karma.conf.js
+++ karma.conf.js
@@ -1,28 +1,21 @@
 module.exports = function (config) {
-  var junitOutputDir = process.env.CIRCLE_TEST_REPORTS || "target/junit"
-
   config.set({
+    autoWatch: true,
+    singleRun: false,
+
     browsers: ['jsdom'],
     basePath: 'target',
-    files: ['karma-test.js'],
+    files: ['ci.js'],
     frameworks: ['cljs-test'],
     plugins: [
-        'karma-jsdom-launcher',
-        'karma-cljs-test',
-        'karma-chrome-launcher',
-        'karma-junit-reporter'
+      'karma-cljs-test',
+      'karma-jsdom-launcher', 
     ],
+    
     colors: true,
     logLevel: config.LOG_INFO,
     client: {
-      args: ['shadow.test.karma.init']
+      args: ["shadow.test.karma.init"],
     },
-
-    // the default configuration
-    junitReporter: {
-      outputDir: junitOutputDir + '/karma', // results will be saved as outputDir/browserName.xml
-      outputFile: undefined, // if included, results will be saved as outputDir/browserName/outputFile
-      suite: '' // suite will become the package name attribute in xml testsuite element
-    }
   })
 }
