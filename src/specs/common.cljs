17423e530b2611c76186f317488a03ee715615b8
yujong-lee <yujonglee@g.skku.edu>
2021-08-04 20:47:56 +0900

    Fix some mistakes I made while transferring this code from another place.
    1. Remove useless spec of field.
    2. Replace :common/ prefix by ::.

--- src/specs/common.cljs
+++ src/specs/common.cljs
@@ -1,14 +1,12 @@
 (ns specs.common
   (:require [cljs.spec.alpha :as spec]))
 
-(spec/def :common/id (spec/and
+(spec/def ::id (spec/and
                       keyword?
                       #(-> %1 name js/parseInt nat-int?)))
 
-(spec/def :common/label string?)
+(spec/def ::label string?)
 
-(spec/def :common/tags (spec/coll-of string? :kind set?))
+(spec/def ::tags (spec/coll-of string? :kind set?))
 
-(spec/def :common/item (spec/keys :req-un [:common/label :common/tags]))
-
-(spec/def :common/field (spec/keys :req-un [:common/label :common/tags]))
+(spec/def ::item (spec/keys :req-un [::label ::tags]))
